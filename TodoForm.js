5a6e7ccb03d70fce7ef52029cb4c172ab1913f07
Ebonygray22 <168474203+Ebonygray22@users.noreply.github.com>
2024-08-13 09:23:55 -0400

    TodoForm.js

---
+++ TodoForm.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const TodoForm = ({ addTodo }) => {
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(title);
+    setTitle('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) =>
